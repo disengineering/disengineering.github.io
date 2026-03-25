31277fc094a54f4ee9961fc0a758c949322b0a63
Max Bo <max@maxbo.me>
2026-03-24 21:20:51 -0400

    Restore

--- calendar/events/events.11tydata.mjs
+++ calendar/events/events.11tydata.mjs
@@ -1,6 +1,6 @@
 export default {
   tags: "calendar",
-  layout: "layouts/calendar-event.njk",
+  layout: "layouts/calendar-event.html",
   eleventyComputed: {
     permalink: (data) => `/calendar/${data.page.fileSlug}/index.html`,
   },
