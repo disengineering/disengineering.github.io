fb77eb7a68adb8c87cd5a3759403b6046afff789
Gib Bernath <gibbernath@gmail.com>
2026-05-03 17:44:35 -0400

    push

--- archive/items/items.11tydata.mjs
+++ archive/items/items.11tydata.mjs
@@ -1,6 +1,7 @@
 export default {
   tags: "archive",
   layout: "layouts/base.html",
+  colorScheme: "hyperblue",
   eleventyComputed: {
     permalink: (data) => `/archive/${data.page.fileSlug}/index.html`,
   },
--- events/items/items.11tydata.mjs
+++ events/items/items.11tydata.mjs
@@ -1,6 +1,7 @@
 export default {
   tags: "event",
   layout: "layouts/event.html",
+  colorScheme: "bsod",
   eleventyComputed: {
     permalink: (data) => `/events/${data.page.fileSlug}/index.html`,
     /** Same shape as collection items so `event-card` can be shared on detail pages. */
